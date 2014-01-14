cd7a891746c16cb778f1f327433d33441e81c0c0
Svyat Kravchenko <svyatoslav.kravchenko@wonga.com>
2014-01-14 16:53:39 +0000

    Adjust logging for main proj

--- src/AutomatedMt4/AutomatedMT4.Main/Program.cs
+++ src/AutomatedMt4/AutomatedMT4.Main/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace AutomatedMT4.Main
+﻿using log4net;
+
+namespace AutomatedMT4.Main
 {
     public class Program
     {
+        private static  readonly ILog _log = LogManager.GetLogger(typeof(Program));
+
         static void Main(string[] args)
         {
-
+            log4net.Config.XmlConfigurator.Configure();
+            _log.DebugFormat("Program was started");
         }
     }
 }
