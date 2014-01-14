ff748c03fa9585009098d3efc5a011ee95c4d304
Svyat Kravchenko <svyatoslav.kravchenko@wonga.com>
2014-01-14 18:17:32 +0000

    Add Quartz. Need to fix build

--- src/AutomatedMt4/AutomatedMT4.Main/Program.cs
+++ src/AutomatedMt4/AutomatedMT4.Main/Program.cs
@@ -1,4 +1,5 @@
-﻿using log4net;
+﻿using AutomatedMT4.Scheduler;
+using log4net;
 
 namespace AutomatedMT4.Main
 {
@@ -10,6 +11,7 @@ namespace AutomatedMT4.Main
         {
             log4net.Config.XmlConfigurator.Configure();
             _log.DebugFormat("Program was started");
+            new SchedulerManager().Start();
         }
     }
 }
---
+++ src/AutomatedMt4/AutomatedMT4.Scheduler/SchedulerManager.cs
@@ -0,0 +1,21 @@
+﻿using log4net;
+using Quartz;
+using Quartz.Impl;
+
+namespace AutomatedMT4.Scheduler
+{
+    public class SchedulerManager
+    {
+        private static readonly ILog _log = LogManager.GetLogger(typeof(SchedulerManager));
+        private IScheduler _scheduler;
+        
+        public void Start()
+        {
+            ISchedulerFactory schedulerFactory = new StdSchedulerFactory();
+            _scheduler = schedulerFactory.GetScheduler();
+            _scheduler.Start();
+            _log.DebugFormat("Start Scheduler");
+
+        }
+    }
+}
