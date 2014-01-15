602f68a13b5b65701e6cd453cc4dc133030ced51
Svyat Kravchenko <svyatoslav.kravchenko@wonga.com>
2014-01-15 11:39:34 +0000

    Adjust logging

--- src/AutomatedMt4/AutomatedMT4.Main/Program.cs
+++ src/AutomatedMt4/AutomatedMT4.Main/Program.cs
@@ -1,15 +1,16 @@
 ﻿using AutomatedMT4.Scheduler;
+using log4net;
 
 namespace AutomatedMT4.Main
 {
     public class Program
     {
-        //private static  readonly ILog _log = LogManager.GetLogger(typeof(Program));
+        private static  readonly ILog _log = LogManager.GetLogger(typeof(Program));
 
         static void Main(string[] args)
         {
-            //log4net.Config.XmlConfigurator.Configure();
-            //_log.DebugFormat("Program was started");
+            log4net.Config.XmlConfigurator.Configure();
+            _log.DebugFormat("Program was started");
             new SchedulerManager().Start();
         }
     }
